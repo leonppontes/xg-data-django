babbcbe953c98a982fdd6ec35cbbf83ace63f053
unknown <leon.pontes@laceti.cin.ufpe.br>
2024-07-14 21:51:00 -0300

    squad xG dataframes added

--- brasileiraoDF.py
+++ brasileiraoDF.py
@@ -17,3 +17,21 @@ keeper_df = keeper_df.sort_values(by='PSxG-GA/90', ascending=False)
 keeper_df['Age'] = keeper_df['Age'].str[:-4]
 keeper_df['Nation'] = keeper_df['Nation'].str[3:]
 print(keeper_df.head())
+
+#Squad xG Dataframes
+urlSquad = 'https://fbref.com/en/comps/24/shooting/Serie-A-Stats'
+squad_df = pd.read_html(urlSquad)[0]
+squadXG_df = squad_df[[('Unnamed: 0_level_0','Squad'),('Unnamed: 2_level_0','90s'),
+                    ('Standard','Gls'),('Expected','xG'),('Expected','G-xG')]]
+squadXG_df.columns = ['Squad', '90s', 'Gls', 'xG', 'G-xG']
+squadXG_df['xG'] = squadXG_df['xG'].astype(float)
+squadXG_df = squadXG_df.sort_values(by='xG', ascending=False)
+print(squadXG_df.head())
+
+squad_XGA_df = pd.read_html(urlSquad)[1]
+squadXGA_df = squad_XGA_df[[('Unnamed: 0_level_0','Squad'),('Unnamed: 2_level_0','90s'),
+                    ('Standard','Gls'),('Expected','xG'),('Expected','G-xG')]]
+squadXGA_df.columns = ['Squad', '90s', 'Gls', 'xG', 'G-xG']
+squadXGA_df['xG'] = squadXGA_df['xG'].astype(float)
+squadXGA_df = squadXGA_df.sort_values(by='xG', ascending=True)
+print(squadXGA_df.head())
